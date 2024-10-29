95c0d844142b22f29b5f0c61f787327060a037fa
realcdugz <connorduggan30@gmail.com>
2024-10-29 12:35:00 -0500

    add exercise solutions

---
+++ store_item.rb
@@ -0,0 +1,6 @@
+item1 = { item_name: "Sony", item_model: "FX3", item_price: 3500 }
+item2 = { item_name: "Red", item_model: "Komodo", item_price: 4995 }
+item3 = { item_name: "Black Magic", item_model: "Pocket 6k", item_price: 2999 }
+puts "#{item1[:item_name]} #{item1[:item_model]} #{item1[:item_price]}"
+puts "#{item2[:item_name]} #{item2[:item_model]} #{item2[:item_price]}"
+puts "#{item3[:item_name]} #{item3[:item_model]} #{item3[:item_price]}"
